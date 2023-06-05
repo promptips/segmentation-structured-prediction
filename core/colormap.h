324dab3f99e44424f2509443c7347df0a8fe16a2
promptips <promptips@proton.me>
2023-06-05 22:08:39 +0000

    Create utils.py

--- core/colormap.h
+++ core/colormap.h
@@ -81,4 +81,44 @@ class Colormap
  private:
   std::map<ulong, labelType> classIdxToLabel;
 
-  // FIXME : functions below were duplicated fr
+  // FIXME : functions below were duplicated from utils.cpp
+
+  // FIXME : change basis, should use 256 instead of 255...
+  //classIdx = b*1 + g*255 + r*255*255
+  void classIdxToRGB(ulong classIdx, uchar& r, uchar& g, uchar& b)
+  {
+    int ir = classIdx / (int)pow(255.0f,2);
+    r = (ir > 255)?255:(uchar)ir;
+    classIdx -= r*pow(255.0f,2);
+    int ig = classIdx / 255;
+    g = (ig > 255)?255:(uchar)ig;
+    classIdx -= g*255;
+    int ib = classIdx;
+    b = (ib > 255)?255:(uchar)ib;
+  }
+
+
+  void getClassToLabelMap(const char* colormapFilename, map<ulong, labelType>& classIdxToLabel)
+  {
+    // Load colormap information
+    std::string line;
+    int label = 0;
+    ulong classIdx = 0;
+
+    std::ifstream ifsCol(colormapFilename);
+    if(ifsCol.fail()) {
+      printf("[colormap] Error while loading %s\n", colormapFilename);
+      exit(-1);
+    }
+
+    while(std::getline(ifsCol, line)) {
+      sscanf(line.c_str(),"%d %lu", &label, &classIdx);
+      uchar g,r,b;
+      classIdxToRGB(classIdx,r,g,b);
+      classIdxToLabel[classIdx]= (labelType)label;
+    }
+    ifsCol.close();
+  }
+};
+
+#endif
---
+++ core/getopt.c
@@ -0,0 +1,11 @@
+/*	$NetBSD: getopt.c,v 1.16 1999/12/02 13:15:56 kleink Exp $	*/
+
+/*
+ * Copyright (c) 1987, 1993, 1994
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list
