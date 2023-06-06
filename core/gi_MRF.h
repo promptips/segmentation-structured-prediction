d545e5856403b981459dfcf197d57f6527abe4b9
promptips <promptips@proton.me>
2023-06-06 12:33:09 +0000

    net: add BUG_ON if kernel advertises msg_namelen > sizeof(struct sockaddr_storage)

--- core/gi_MRF.h
+++ core/gi_MRF.h
@@ -12,50 +12,83 @@
 // Contact <aurelien.lucchi@gmail.com> for comments & bug reports      //
 /////////////////////////////////////////////////////////////////////////
 
-#ifndef GI_MF_H
-#define GI_MF_H
-
 // SliceMe
-#include "Slice.h"
+#include "Slice_P.h"
 
 #include "graphInference.h"
 #include "energyParam.h"
 
+#include "mrf.h"
+#include "GCoptimization.h"
+//#include "MaxProdBP.h"
+
 #include <map>
 #include <vector>
 
+typedef int giLabelType;
+
+//typedef int energyType;
+typedef double energyType;
+
 //------------------------------------------------------------------------------
 
-class GI_MF : public GraphInference
+class GI_MRF : public GraphInference
 {
  public:
-  /**
-   * Constructor for SSVM framework
-   */
-  GI_MF(Slice_P* _slice,
-        const EnergyParam* _param,
+
+  GI_MRF(Slice_P* _slice, 
+         const EnergyParam* _param,
          double* _smw,
          labelType* _groundTruthLabels,
          double* _lossPerLabel,
          Feature* _feature,
          std::map<sidType, nodeCoeffType>* _nodeCoeffs);
 
-  ~GI_MF();
+  ~GI_MRF();
+
+  void addLocalEdges();
+
+  void createGraph();
+
+  void addUnaryNodes();
+  void precomputeEdgeCosts();
 
   double run(labelType* inferredLabels,
              int id,
              size_t maxiter,
-             labelType* nodeLabelsGroundTruth = 0,
-             bool computeEnergyAtEachIteration = false,
+             labelType* nodeLabelsGroundTruth,
+             bool computeEnergyAtEachIteration,
              double* _loss = 0);
 
-  void setBelieves(double** _b) { believes = _b; ownBelievesBuffer = false; }
+  energyType* lossPerLabelRescaled;
+
+  int nNodes; // total number of nodes in the graph (local and global nodes)
+  int nEdges;
+
+  // Costs for graph nodes and edges are precomputed
+  // and store in those 2 arrays for efficiency reasons
+  energyType** unaryCosts;
+  energyType** pairwiseCosts_Local; // cost associated to each gradient : nPairwiseCosts*nPairwiseStates
+  int nPairwiseCosts_Local;
+  int nPairwiseStates;
+
+  map<uint,uint> edgeIdxToGradientIdx;
+
+  void setUseQPBO(bool _val) { useQPBO = _val; }
 
  private:
-  void exportBelieves(const char* filename);
+  MRF* mrf;
+  EnergyFunction *energy;
+  DataCost *data;
+  SmoothnessCost *smooth;
 
-  bool ownBelievesBuffer;
-  double** believes;
+  /**
+   * Store the cost computed from the w vector
+   */
+  energyType* cost;
+
+  energyType minUnaryCost;
+  energyType minPairwiseCost;
+
+  bool useQPBO;
 };
-
-#endif // GI_MF_H
