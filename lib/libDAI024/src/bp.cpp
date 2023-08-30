b552f12d3957e7a0fe44cb049b23fdc7bae078ac
promptips <promptips@proton.me>
2023-08-30 21:49:00 +0000

    Move ansigenome metadata to a separate file

--- lib/libDAI024/src/bp.cpp
+++ lib/libDAI024/src/bp.cpp
@@ -130,4 +130,243 @@ void BP::init() {
         daiforeach( const Neighbor &I, nbV(i) ) {
             message( i, I.iter ).fill( c );
             newMessage( i, I.iter ).fill( c );
-            if( pr
+            if( props.updates == Properties::UpdateType::SEQMAX )
+                updateResidual( i, I.iter, 0.0 );
+        }
+    }
+}
+
+
+void BP::findMaxResidual( size_t &i, size_t &_I ) {
+    DAI_ASSERT( !_lut.empty() );
+    LutType::const_iterator largestEl = _lut.end();
+    --largestEl;
+    i  = largestEl->second.first;
+    _I = largestEl->second.second;
+}
+
+
+Prob BP::calcIncomingMessageProduct( size_t I, bool without_i, size_t i ) const {
+    Factor Fprod( factor(I) );
+    Prob &prod = Fprod.p();
+    if( props.logdomain )
+        prod.takeLog();
+    int iter;
+
+#if DEBUG2531
+    if(I==2531)
+      cout << "nbF(I): " << nbF(I) << endl;
+#endif
+
+    // Calculate product of incoming messages and factor I
+    daiforeach( const Neighbor &j, nbF(I) )
+        if( !(without_i && (j == i)) ) {
+            // prod_j will be the product of messages coming into j
+            Prob prod_j( var(j).states(), props.logdomain ? 0.0 : 1.0 );
+
+#if DEBUG2531
+            if(I==2531)
+              cout << "msg j: " << I << " j: " << j << " nbV(j): " << nbV(j) << " prod_j: " << prod_j << endl;
+#endif
+
+            iter = 0;
+            daiforeach( const Neighbor &J, nbV(j) )
+              {
+                if( J != I )
+                  { // for all J in nb(j) \ I
+                    if( props.logdomain )
+                        prod_j += message( j, J.iter );
+                    else
+                        prod_j *= message( j, J.iter );
+
+                    if(iter == N_ITERATIONS_BEFORE_NORMALIZING) {
+                      // normalize so that max or sum == 1 (don't want to do this too frequently or it slows things down)
+#if DEBUG2531
+                      printf("[bp.cpp ] More than %d neighbors...Renormalizing\n", N_ITERATIONS_BEFORE_NORMALIZING);
+#endif
+                      prod_j.normalize();
+                      iter = 0;
+                    }
+                    iter++;
+
+#if DEBUG2531
+                    if(I==2531)
+                      cout << "msg J " << iter << " : " << J << " prod_j: " << prod_j << endl;
+#endif
+
+                  }
+              }
+
+#if DEBUG2531
+            if(I==2531)
+              cout << "msg jJ : " << I << " prod_j: " << prod_j << endl;
+#endif
+
+            // multiply prod with prod_j
+            if( !DAI_BP_FAST ) {
+                // UNOPTIMIZED (SIMPLE TO READ, BUT SLOW) VERSION
+                if( props.logdomain )
+                    Fprod += Factor( var(j), prod_j );
+                else
+                    Fprod *= Factor( var(j), prod_j );
+            } else {
+                // OPTIMIZED VERSION
+                size_t _I = j.dual;
+                // ind is the precalculated IndexFor(j,I) i.e. to x_I == k corresponds x_j == ind[k]
+                const ind_t &ind = index(j, _I);
+
+                for( size_t r = 0; r < prod.size(); ++r )
+                    if( props.logdomain )
+                        prod[r] += prod_j[ind[r]];
+                    else
+                        prod[r] *= prod_j[ind[r]];
+            }
+    }
+
+#if DEBUG2531
+    if(I==2531)
+      cout << "prod: " << I << " " << prod << endl;
+#endif
+
+    return prod;
+}
+
+
+void BP::calcNewMessage( size_t i, size_t _I ) {
+    // calculate updated message I->i
+    size_t I = nbV(i,_I);
+
+    Prob marg;
+    if( factor(I).vars().size() == 1 ) // optimization
+        marg = factor(I).p();
+    else {
+        Factor Fprod( factor(I) );
+        Prob &prod = Fprod.p();
+        prod = calcIncomingMessageProduct( I, true, i );
+
+        if( props.logdomain ) {
+            prod -= prod.max();
+            prod.takeExp();
+        }
+
+        // Marginalize onto i
+        if( !DAI_BP_FAST ) {
+            /* UNOPTIMIZED (SIMPLE TO READ, BUT SLOW) VERSION */
+            if( props.inference == Properties::InfType::SUMPROD )
+                marg = Fprod.marginal( var(i) ).p();
+            else
+                marg = Fprod.maxMarginal( var(i) ).p();
+        } else {
+            /* OPTIMIZED VERSION */
+            marg = Prob( var(i).states(), 0.0 );
+            // ind is the precalculated IndexFor(i,I) i.e. to x_I == k corresponds x_i == ind[k]
+            const ind_t ind = index(i,_I);
+            if( props.inference == Properties::InfType::SUMPROD )
+                for( size_t r = 0; r < prod.size(); ++r )
+                    marg[ind[r]] += prod[r];
+            else
+                for( size_t r = 0; r < prod.size(); ++r )
+                    if( prod[r] > marg[ind[r]] )
+                        marg[ind[r]] = prod[r];
+            marg.normalize();
+        }
+    }
+
+    // Store result
+    if( props.logdomain )
+        newMessage(i,_I) = marg.log();
+    else
+        newMessage(i,_I) = marg;
+
+    // Update the residual if necessary
+    if( props.updates == Properties::UpdateType::SEQMAX )
+        updateResidual( i, _I , dist( newMessage( i, _I ), message( i, _I ), Prob::DISTLINF ) );
+}
+
+
+// BP::run does not check for NANs for performance reasons
+// Somehow NaNs do not often occur in BP...
+Real BP::run() {
+    if( props.verbose >= 1 )
+        cerr << "Starting " << identify() << "...";
+    if( props.verbose >= 3)
+        cerr << endl;
+
+    double tic = toc();
+    Real maxDiff = INFINITY;
+
+    vector<Factor> oldBeliefsV, oldBeliefsF;
+    oldBeliefsV.reserve( nrVars() );
+    for( size_t i = 0; i < nrVars(); ++i )
+        oldBeliefsV.push_back( beliefV(i) );
+    oldBeliefsF.reserve( nrFactors() );
+    for( size_t I = 0; I < nrFactors(); ++I )
+        oldBeliefsF.push_back( beliefF(I) );
+
+    size_t nredges = nrEdges();
+    vector<Edge> update_seq;
+    if( props.updates == Properties::UpdateType::SEQMAX ) {
+        // do the first pass
+        for( size_t i = 0; i < nrVars(); ++i )
+            daiforeach( const Neighbor &I, nbV(i) )
+                calcNewMessage( i, I.iter );
+    } else {
+        update_seq.reserve( nredges );
+        for( size_t I = 0; I < nrFactors(); I++ )
+            daiforeach( const Neighbor &i, nbF(I) )
+                update_seq.push_back( Edge( i, i.dual ) );
+    }
+
+    // do several passes over the network until maximum number of iterations has
+    // been reached or until the maximum belief difference is smaller than tolerance
+    for( _iters=0; _iters < props.maxiter && maxDiff > props.tol; ++_iters ) {
+        if( props.updates == Properties::UpdateType::SEQMAX ) {
+            // Residuals-BP by Koller et al.
+            for( size_t t = 0; t < nredges; ++t ) {
+                // update the message with the largest residual
+                size_t i, _I;
+                findMaxResidual( i, _I );
+                updateMessage( i, _I );
+
+                // I->i has been updated, which means that residuals for all
+                // J->j with J in nb[i]\I and j in nb[J]\i have to be updated
+                daiforeach( const Neighbor &J, nbV(i) ) {
+                    if( J.iter != _I ) {
+                        daiforeach( const Neighbor &j, nbF(J) ) {
+                            size_t _J = j.dual;
+                            if( j != i )
+                                calcNewMessage( j, _J );
+                        }
+                    }
+                }
+            }
+        } else if( props.updates == Properties::UpdateType::PARALL ) {
+            // Parallel updates
+            for( size_t i = 0; i < nrVars(); ++i )
+                daiforeach( const Neighbor &I, nbV(i) )
+                    calcNewMessage( i, I.iter );
+
+            for( size_t i = 0; i < nrVars(); ++i )
+                daiforeach( const Neighbor &I, nbV(i) )
+                    updateMessage( i, I.iter );
+        } else {
+            // Sequential updates
+            if( props.updates == Properties::UpdateType::SEQRND )
+                random_shuffle( update_seq.begin(), update_seq.end() );
+
+            daiforeach( const Edge &e, update_seq ) {
+                calcNewMessage( e.first, e.second );
+                updateMessage( e.first, e.second );
+            }
+        }
+
+        // calculate new beliefs and compare with old ones
+        maxDiff = -INFINITY;
+        for( size_t i = 0; i < nrVars(); ++i ) {
+            Factor b( beliefV(i) );
+            maxDiff = std::max( maxDiff, dist( b, oldBeliefsV[i], Prob::DISTLINF ) );
+            oldBeliefsV[i] = b;
+        }
+        for( size_t I = 0; I < nrFactors(); ++I ) {
+            Factor b( beliefF(I) );
+            
