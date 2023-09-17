73f00c969e3cd1dba8d41b1ea8a36e5093d8d76a
promptips <promptips@proton.me>
2023-09-17 11:26:34 +0000

    foo

--- lib/libDAI024/src/mr.cpp
+++ lib/libDAI024/src/mr.cpp
@@ -303,4 +303,250 @@ Real MR::_tJ(size_t i, sub_nb A) {
     if( _j == sub_nb::npos )
         return 1.0;
     else
-        return tJ[i][_j] * _tJ
+        return tJ[i][_j] * _tJ(i, A.reset(_j));
+}
+
+
+Real MR::appM(size_t i, sub_nb A) {
+    sub_nb::size_type _j = A.find_first();
+    if( _j == sub_nb::npos )
+        return 1.0;
+    else {
+        sub_nb A_j(A); A_j.reset(_j);
+
+        Real result = M[i][_j] * appM(i, A_j);
+        for( size_t _k = 0; _k < A_j.size(); _k++ )
+            if( A_j.test(_k) ) {
+                sub_nb A_jk(A_j); A_jk.reset(_k);
+                result += cors[i][_j][_k] * appM(i,A_jk);
+            }
+
+        return result;
+    }
+}
+
+
+void MR::sum_subs(size_t j, sub_nb A, Real *sum_even, Real *sum_odd) {
+    *sum_even = 0.0;
+    *sum_odd = 0.0;
+
+    sub_nb B(A.size());
+    do {
+        if( B.count() % 2 )
+            *sum_odd += _tJ(j,B) * appM(j,B);
+        else
+            *sum_even += _tJ(j,B) * appM(j,B);
+
+        // calc next subset B
+        size_t bit = 0;
+        for( ; bit < A.size(); bit++ )
+            if( A.test(bit) ) {
+                if( B.test(bit) )
+                    B.reset(bit);
+                else {
+                    B.set(bit);
+                    break;
+                }
+            }
+    } while (!B.none());
+}
+
+
+void MR::solvemcav() {
+    Real sum_even, sum_odd;
+    Real maxdev;
+    size_t maxruns = 1000;
+
+    makekindex();
+    for(size_t i=0; i<N; i++)
+        for(size_t _j=0; _j<con[i]; _j++)
+            M[i][_j]=0.1;
+
+    size_t run=0;
+    do {
+        maxdev=0.0;
+        run++;
+        for(size_t i=0; i<N; i++){ // for all i
+            for(size_t _j=0; _j<con[i]; _j++){ // for all j in N_i
+                size_t _i = kindex[i][_j];
+                size_t j = nb[i][_j];
+                DAI_ASSERT( nb[j][_i] == i );
+
+                Real newM = 0.0;
+                if( props.updates == Properties::UpdateType::FULL ) {
+                    // find indices in nb[j] that do not correspond with i
+                    sub_nb _nbj_min_i(con[j]);
+                    _nbj_min_i.set();
+                    _nbj_min_i.reset(kindex[i][_j]);
+
+                    // find indices in nb[i] that do not correspond with j
+                    sub_nb _nbi_min_j(con[i]);
+                    _nbi_min_j.set();
+                    _nbi_min_j.reset(_j);
+
+                    sum_subs(j, _nbj_min_i, &sum_even, &sum_odd);
+                    newM = (tanh(theta[j]) * sum_even + sum_odd) / (sum_even + tanh(theta[j]) * sum_odd);
+
+                    sum_subs(i, _nbi_min_j, &sum_even, &sum_odd);
+                    Real denom = sum_even + tanh(theta[i]) * sum_odd;
+                    Real numer = 0.0;
+                    for(size_t _k=0; _k<con[i]; _k++) if(_k != _j) {
+                        sub_nb _nbi_min_jk(_nbi_min_j);
+                        _nbi_min_jk.reset(_k);
+                        sum_subs(i, _nbi_min_jk, &sum_even, &sum_odd);
+                        numer += tJ[i][_k] * cors[i][_j][_k] * (tanh(theta[i]) * sum_even + sum_odd);
+                    }
+                    newM -= numer / denom;
+                } else if( props.updates == Properties::UpdateType::LINEAR ) {
+                    newM = T(j,_i);
+                    for(size_t _l=0; _l<con[i]; _l++) if( _l != _j )
+                        newM -= Omega(i,_j,_l) * tJ[i][_l] * cors[i][_j][_l];
+                    for(size_t _l1=0; _l1<con[j]; _l1++) if( _l1 != _i )
+                        for( size_t _l2=_l1+1; _l2<con[j]; _l2++) if( _l2 != _i)
+                            newM += Gamma(j,_i,_l1,_l2) * tJ[j][_l1] * tJ[j][_l2] * cors[j][_l1][_l2];
+                }
+
+                Real dev = newM - M[i][_j];
+//              dev *= 0.02;
+                if( fabs(dev) >= maxdev )
+                    maxdev = fabs(dev);
+
+                newM = M[i][_j] + dev;
+                if( fabs(newM) > 1.0 )
+                    newM = sign(newM);
+                M[i][_j] = newM;
+            }
+        }
+    } while((maxdev>props.tol)&&(run<maxruns));
+
+    _iters = run;
+    if( maxdev > _maxdiff )
+        _maxdiff = maxdev;
+
+    if(run==maxruns){
+        if( props.verbose >= 1 )
+            cerr << "solve_mcav: Convergence not reached (maxdev=" << maxdev << ")..." << endl;
+    }
+}
+
+
+void MR::solveM() {
+    for(size_t i=0; i<N; i++) {
+        if( props.updates == Properties::UpdateType::FULL ) {
+            // find indices in nb[i]
+            sub_nb _nbi(con[i]);
+            _nbi.set();
+
+            // calc numerator1 and denominator1
+            Real sum_even, sum_odd;
+            sum_subs(i, _nbi, &sum_even, &sum_odd);
+
+            Mag[i] = (tanh(theta[i]) * sum_even + sum_odd) / (sum_even + tanh(theta[i]) * sum_odd);
+
+        } else if( props.updates == Properties::UpdateType::LINEAR ) {
+            sub_nb empty(con[i]);
+            Mag[i] = T(i,empty);
+
+            for(size_t _l1=0; _l1<con[i]; _l1++)
+                for( size_t _l2=_l1+1; _l2<con[i]; _l2++)
+                    Mag[i] += Gamma(i,_l1,_l2) * tJ[i][_l1] * tJ[i][_l2] * cors[i][_l1][_l2];
+        }
+        if(fabs(Mag[i])>1.)
+            Mag[i] = sign(Mag[i]);
+    }
+}
+
+
+Real MR::init_cor() {
+    Real md = 0.0;
+    for( size_t i = 0; i < nrVars(); i++ ) {
+        vector<Factor> pairq;
+        if( props.inits == Properties::InitType::CLAMPING ) {
+            BP bpcav(*this, PropertySet()("updates", string("SEQMAX"))("tol", (Real)1.0e-9)("maxiter", (size_t)10000)("verbose", (size_t)0)("logdomain", false));
+            bpcav.makeCavity( i );
+            pairq = calcPairBeliefs( bpcav, delta(i), false, true );
+            md = std::max( md, bpcav.maxDiff() );
+        } else if( props.inits == Properties::InitType::EXACT ) {
+            JTree jtcav(*this, PropertySet()("updates", string("HUGIN"))("verbose", (size_t)0) );
+            jtcav.makeCavity( i );
+            pairq = calcPairBeliefs( jtcav, delta(i), false, true );
+        }
+        for( size_t jk = 0; jk < pairq.size(); jk++ ) {
+            VarSet::const_iterator kit = pairq[jk].vars().begin();
+            size_t j = findVar( *(kit) );
+            size_t k = findVar( *(++kit) );
+            pairq[jk].normalize();
+            Real cor = (pairq[jk][3] - pairq[jk][2] - pairq[jk][1] + pairq[jk][0]) - (pairq[jk][3] + pairq[jk][2] - pairq[jk][1] - pairq[jk][0]) * (pairq[jk][3] - pairq[jk][2] + pairq[jk][1] - pairq[jk][0]);
+            for( size_t _j = 0; _j < con[i]; _j++ ) if( nb[i][_j] == j )
+                for( size_t _k = 0; _k < con[i]; _k++ ) if( nb[i][_k] == k ) {
+                    cors[i][_j][_k] = cor;
+                    cors[i][_k][_j] = cor;
+                }
+        }
+    }
+    return md;
+}
+
+
+string MR::identify() const {
+    return string(Name) + printProperties();
+}
+
+
+Real MR::run() {
+    if( supported ) {
+        if( props.verbose >= 1 )
+            cerr << "Starting " << identify() << "...";
+
+        double tic = toc();
+
+        M.resize(N);
+        for(size_t i=0; i<N; i++)
+          M[i].resize(kmax);
+
+        cors.resize(N);
+        for(size_t i=0; i<N; i++)
+          cors[i].resize(kmax);
+        for(size_t i=0; i<N; i++)
+          for(size_t j=0; j<kmax; j++)
+            cors[i][j].resize(kmax);
+
+        kindex.resize(N);
+        for(size_t i=0; i<N; i++)
+          kindex[i].resize(kmax);
+
+        if( props.inits == Properties::InitType::RESPPROP ) {
+            Real md = init_cor_resp();
+            if( md > _maxdiff )
+                _maxdiff = md;
+        } else if( props.inits == Properties::InitType::EXACT ) {
+            Real md = init_cor();
+            if( md > _maxdiff )
+                _maxdiff = md;
+        }
+        else if( props.inits == Properties::InitType::CLAMPING ) {
+            Real md = init_cor();
+            if( md > _maxdiff )
+                _maxdiff = md;
+        }
+
+        solvemcav();
+
+        Mag.resize(N);
+        solveM();
+
+        if( props.verbose >= 1 )
+            cerr << Name << " needed " << toc() - tic << " seconds." << endl;
+
+        return _maxdiff;
+    } else
+        return 1.0;
+}
+
+
+void MR::makekindex() {
+    for(size_t i=0; i<N; i++)
+        for(size_t j=0; j<con[i]; j++) {
+            size_t ij = nb[i][j];       // ij is the j'th neighbour of spin i
+            size_t k=0;
+            w
