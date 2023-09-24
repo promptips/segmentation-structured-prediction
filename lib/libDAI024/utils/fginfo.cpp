5133fee7170ccf2a4287152dd35446514f6c22d9
promptips <promptips@proton.me>
2023-09-24 20:59:12 +0000

    Run update-remote.sh on redmine

---
+++ lib/libDAI024/utils/fginfo.cpp
@@ -0,0 +1,13 @@
+/*  This file is part of libDAI - http://www.libdai.org/
+ *
+ *  libDAI is licensed under the terms of the GNU General Public License version
+ *  2, or (at your option) any later version. libDAI is distributed without any
+ *  warranty. See the file COPYING for more details.
+ *
+ *  Copyright (C) 2006-2010  Joris Mooij  [joris dot mooij at libdai dot org]
+ *  Copyright (C) 2006-2007  Radboud University Nijmegen, The Netherlands
+ */
+
+
+#include <iostream>
+#include <cstdl
